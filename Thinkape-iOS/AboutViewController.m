a853133a05eee9d0a8ca22f6ea2a1f716758a2a5
chenkai1 <admin@admindemacbook-pro.local>
2016-03-07 15:22:25 +0800

    AboutViewController的修改

--- Thinkape-iOS/AboutViewController.m
+++ Thinkape-iOS/AboutViewController.m
@@ -8,8 +8,23 @@
 
 #import "AboutViewController.h"
 #import "MacroDefinition.h"
+
 @interface AboutViewController ()
 
+
+{
+    
+    UIScrollView *_myScrollView;
+    UIImageView *_thinkApeImage;
+    
+    UILabel *_thinkApeLabel;
+    UILabel *_openLabel;
+    UILabel *_urlLabel;
+    UILabel *_weixinLabel;
+    UIImageView *_erweimaImage;
+    UILabel *_thinkLabel;
+    
+}
 
 @end
 
@@ -18,7 +33,70 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [self createUI];
 }
+
+
+- (void) createUI
+{
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    
+    _myScrollView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 64, SCREEN_WIDTH, SCREEN_HEIGHT)];
+    
+    //
+    _thinkApeImage = [[UIImageView alloc]initWithFrame:CGRectMake((SCREEN_WIDTH-100)/2, 50, 100, 100)];
+    _thinkApeImage.image = [UIImage imageNamed:@"logo_thinkape_show"];
+    
+    //
+    _thinkApeLabel = [[UILabel alloc]initWithFrame:CGRectMake(10, 200, SCREEN_WIDTH-20, 60)];
+    _thinkApeLabel.text = @"思凯普";
+    _thinkApeLabel.font = [UIFont systemFontOfSize:20];
+    _thinkApeLabel.textAlignment = NSTextAlignmentCenter;
+    
+    //
+    _openLabel = [[UILabel alloc]initWithFrame:CGRectMake(10, 300, SCREEN_WIDTH-20, 50)];
+    _openLabel.text = @"开启专业、高效、共享、整合的新一代全面预算与费用管控平台";
+    _openLabel.textAlignment = NSTextAlignmentCenter;
+    _openLabel.numberOfLines = 0;
+    
+    //
+    _urlLabel = [[UILabel alloc]initWithFrame:CGRectMake(10, 350, SCREEN_WIDTH-20, 50)];
+    _urlLabel.text = @"思凯普官网：www.thinkape.com.cn";
+    _urlLabel.textAlignment = NSTextAlignmentCenter;
+    
+    //
+    _weixinLabel = [[UILabel alloc]initWithFrame:CGRectMake(10, 400, SCREEN_WIDTH-20, 50)];
+    _weixinLabel.text = @"思凯普微信公众号：Thinkape思凯普";
+    _weixinLabel.textAlignment = NSTextAlignmentCenter;
+    
+    //
+    _erweimaImage = [[UIImageView alloc]initWithFrame:CGRectMake((SCREEN_WIDTH-200)/2, 500, 200, 200)];
+    _erweimaImage.image = [UIImage imageNamed:@"Thinkapeimage"];
+    
+    //
+    _thinkLabel = [[UILabel alloc]initWithFrame:CGRectMake(10, 760, SCREEN_WIDTH-20, 50)];
+    _thinkLabel.text = @"Thinkape 1998-2016 思凯普中国 版权所有";
+    _thinkLabel.font = [UIFont systemFontOfSize:14];
+    _thinkLabel.numberOfLines = 0;
+    _thinkLabel.textAlignment = NSTextAlignmentCenter;
+    
+    
+    [_myScrollView addSubview:_thinkApeImage];
+    [_myScrollView addSubview:_thinkApeLabel];
+    [_myScrollView addSubview:_openLabel];
+    [_myScrollView addSubview:_urlLabel];
+    [_myScrollView addSubview:_weixinLabel];
+    [_myScrollView addSubview:_erweimaImage];
+    [_myScrollView addSubview:_thinkLabel];
+    
+    
+    _myScrollView.contentSize = CGSizeMake(SCREEN_WIDTH, 900);
+    
+    [self.view addSubview:_myScrollView];
+    
+    
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
